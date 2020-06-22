70f743fce51b3e973eed862f54127694a87745a9
Guillaume Laine <guillaumelaine2.0@gmail.com>
2020-06-22 18:00:56 +0200

    retrieve artist pic from deezer api

--- app/javascript/components/Session.js
+++ app/javascript/components/Session.js
@@ -1,28 +1,49 @@
 import React from "react"
-import { List, Divider } from 'semantic-ui-react'
+import axios from "axios"
+import { List, Divider, Image } from 'semantic-ui-react'
 
 class Session extends React.Component {
 
+  state = {
+    artistpic: "yo"
+  };
 
+  componentDidMount() {
+    axios.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${this.props.session.band}`)
+    .then((response) => {
+      console.log(response.data);
+      console.log(response.data.data[0].artist.picture_big);
+      this.setState({artistpic: response.data.data[0].artist.picture_big })
+    });
+  }
 
   render() {
 
     const session = this.props.session;
+
     const styles = {
       container: {
-        padding: "3em !important;"
+        padding: "3em !important"
+      },
+      player: {
+        opacity: "0.8",
+        backgroundImage: `url(${this.state.artistpic})`
       }
     };
 
 
     return (
       <div className="session" style={styles.container}>
-        <div class="ui header">{session.band}</div>
-        <div class="ui header">{session.year}</div>
-        <div className="ui embed">
-          <iframe title="video player" src={session.link} />
+        <div className="ui header">{session.band}</div>
+        <div className="ui header">{session.year}</div>
+
+        <div className="ui embed"  >
+          <iframe title="video player" className="videoplayer" src={session.link} style={styles.player} controls="true" >
+            <Image src={this.state.artistpic} fluid />
+          </iframe>
+        </div>
       </div>
-      </div>
+
     );
   }
 }
